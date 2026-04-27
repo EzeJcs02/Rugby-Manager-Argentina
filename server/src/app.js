285fd4a363db04d7cc0f925abe3d742e03bc651b
EzeJcs02 <austerlitzezequiel02@gmail.com>
2026-04-26 22:54:58 -0300

    fix: cors para vercel

--- server/src/app.js
+++ server/src/app.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors({
-  origin: process.env.CLIENT_URL ? [process.env.CLIENT_URL, 'http://localhost:5173'] : true,
+  origin: ['https://rugby-manager-argentina.vercel.app', 'http://localhost:5173'],
 }));
 app.use(express.json());
 
